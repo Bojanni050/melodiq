eda393740babb5cc4aa62e94025567a3ad44d1fc
Dr. Avash Newar <bojan.vanderheide@gmail.com>
2026-06-23 07:23:52 +0200

    fix: use reflection for setSpatialNavigationEnabled (removed in API 33)

--- android/app/src/main/java/nl/melodiq/app/MainActivity.java
+++ android/app/src/main/java/nl/melodiq/app/MainActivity.java
@@ -3,12 +3,16 @@ package nl.melodiq.app;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
+import java.lang.reflect.Method;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebSettings settings = getBridge().getWebView().getSettings();
-        settings.setSpatialNavigationEnabled(true);
+        try {
+            WebSettings settings = getBridge().getWebView().getSettings();
+            Method m = settings.getClass().getMethod("setSpatialNavigationEnabled", boolean.class);
+            m.invoke(settings, true);
+        } catch (Exception ignored) {}
     }
 }
